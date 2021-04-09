2e6fdb463d583986b36c83a96eae031067f98742
BIT Student <classroom@bgit.rs>
2021-04-09 16:59:15 +0200

    style

--- src/pages/CandidateDetails/CandidateDetails.js
+++ src/pages/CandidateDetails/CandidateDetails.js
@@ -1,82 +1,95 @@
-import './CandidateDetails.scss';
-import React, { useContext, useState } from 'react'
-import { candidatesContext } from '../../App';
-import { reportsContext } from '../../App'
-import Modal from '../../components/Modal/Modal';
-import { Link } from 'react-router-dom';
+import "./CandidateDetails.scss";
+import React, { useContext, useState } from "react";
+import { candidatesContext } from "../../App";
+import { reportsContext } from "../../App";
+import Modal from "../../components/Modal/Modal";
+import { Link } from "react-router-dom";
 
 const CandidateDetails = (props) => {
-    const candidates = useContext(candidatesContext);
-    const reportsDetail = useContext(reportsContext);
+  const candidates = useContext(candidatesContext);
+  const reportsDetail = useContext(reportsContext);
 
-    const [modal, setModal] = useState(false);
-    const [report, setReport] = useState(null);
+  const [modal, setModal] = useState(false);
+  const [report, setReport] = useState(null);
 
-    const profile = candidates.find(e => e.id == props.match.params.id);
-    const reportsData = reportsDetail.filter(e => e.candidateId == profile.id);
+  const profile = candidates.find((e) => e.id == props.match.params.id);
+  const reportsData = reportsDetail.filter((e) => e.candidateId == profile.id);
 
-    const showModal = (rep) => {
-        setModal(!modal);
-        setReport(rep);
-    }
+  const showModal = (rep) => {
+    setModal(!modal);
+    setReport(rep);
+  };
 
-    const closeModal = () => {
-        setModal(false);
-    }
+  const closeModal = () => {
+    setModal(false);
+  };
 
-    const getRealDate = (e) => {
-        let realDate = new Date(e);
-        let date = realDate.getDate();
-        let month = realDate.getMonth() + 1;
-        let year = realDate.getFullYear();
+  const getRealDate = (e) => {
+    let realDate = new Date(e);
+    let date = realDate.getDate();
+    let month = realDate.getMonth() + 1;
+    let year = realDate.getFullYear();
 
-        return (`${date}.${month < 10 ? `0${month}` : `${month}`}.${year}`)
-    }
-    if (!profile) {
-        return null
-    }
-    return (
-        <>
-            <div className="Header">
-                <div className="Logo">Interview Reports</div>
-                <Link className="back-btn" to='/'>Back</Link>
-            </div>
+    return `${date}.${month < 10 ? `0${month}` : `${month}`}.${year}`;
+  };
+  if (!profile) {
+    return null;
+  }
+  return (
+    <>
+      <div className="Header">
+        <div className="Logo">Interview Reports</div>
+        <Link className="back-btn" to="/">
+          Back
+        </Link>
+      </div>
 
-            <div className="CandidateDetails">
-                <img src={profile.avatar} />
-                <div>
-                    <h3>Name: {profile.name}</h3>
-                    <h3>Email: {profile.email}</h3>
-                </div>
-                <div>
-                    <h3>Date of Birth: {getRealDate(profile.birthday)}</h3>
-                    <h3>Education:{profile.education}</h3>
-                </div>
+      <div className="CandidateDetails">
+        <div className="Info">
+          <img src={profile.avatar} />
+          <div>
+            <p>Name: </p>
+            <h3>{profile.name}</h3>
+            <p>Email: </p>
+            <h3>{profile.email}</h3>
+          </div>
+          <div>
+            <p>Date of Birth: </p>
+            <h3>{getRealDate(profile.birthday)}</h3>
+            <p>Education: </p>
+            <h3>{profile.education}</h3>
+          </div>
+        </div>
 
-                <div className="Table">
-                    <h3>Reports</h3>
-                </div>
-                <table className="TableGrid">
-                    <tr>
-                        <th>Company</th>
-                        <th>Inteview date</th>
-                        <th colSpan="2">Status</th>
-                    </tr>
-                    {reportsData.map(e =>
-                        <tr>
-                            <td>{e.companyName}</td>
-                            <td>{getRealDate(e.interviewDate)}</td>
-                            <td>{e.status}</td>
-                            <td><span onClick={() => showModal(e)}>M</span></td>
-                        </tr>)}
-                </table>
-                <Modal modal={modal}
-                    reportsData={report}
-                    handleClose={closeModal}
-                    getRealDate={getRealDate} />
-            </div>
-        </>
-    )
-}
+        <div className="Table">
+          <h3>Reports</h3>
+        </div>
+        <table className="TableGrid">
+          <tr>
+            <th>Company</th>
+            <th>Inteview date</th>
+            <th colSpan="2">Status</th>
+          </tr>
+          {reportsData.map((e) => (
+            <tr>
+              <td>{e.companyName}</td>
+              <td>{getRealDate(e.interviewDate)}</td>
+              <td>{e.status}</td>
+              <td>
+                <span onClick={() => showModal(e)}>&#128065;</span>
+              </td>
+            </tr>
+          ))}
+        </table>
+        <Modal
+          modal={modal}
+          reportsData={report}
+          handleClose={closeModal}
+          getRealDate={getRealDate}
+        />
+      </div>
+    </>
+  );
+};
 
 export default CandidateDetails;
--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -13,35 +13,52 @@ const LogIn = () => {
     console.log(password);
     const url = "http://localhost:3333/login";
     fetch(url, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
         email: email,
         password: password,
-      })
-    }).then(data => data.json())
-      .then(data => setToken(data.accessToken))
-  }
+      }),
+    })
+      .then((data) => data.json())
+      .then((data) => setToken(data.accessToken));
+  };
 
   return (
-
     <form onSubmit={submitHandler}>
       <div className="form-inner">
-        <h2>Login</h2>
+        <h2>Log in</h2>
 
         <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" onChange={e =>
-            setDetails({ ...details, email: e.target.value })} value={email} />
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setDetails({ ...details, email: e.target.value })}
+            value={email}
+          />
         </div>
         <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password" id="password" onChange={e =>
-            setDetails({ ...details, password: e.target.value })} value={password} />
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={password}
+          />
         </div>
-        <input type="submit" value="LOGIN" onClick={submitHandler} />
+        <input
+          className="submit"
+          type="submit"
+          value="LOG IN"
+          onClick={submitHandler}
+        />
       </div>
     </form>
-  )
-}
+  );
+};
 
 export default LogIn;
