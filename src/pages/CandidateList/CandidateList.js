cae6f5c358c6726dbc34ea6b22404a9e180b6b17
Jelena_Andjelkovic <69766559+Jelena-Andjelkovic@users.noreply.github.com>
2021-04-10 11:27:29 +0200

    style (#4)
    * style
    
    * style
    
    Co-authored-by: BIT Student <classroom@bgit.rs>

--- src/pages/CandidateList/CandidateList.js
+++ src/pages/CandidateList/CandidateList.js
@@ -1,34 +1,36 @@
-import React, { useContext } from 'react'
-import './CandidateList.scss';
-import CandidateProfile from '../../components/CandidateProfile/CandidateProfile'
+import React, { useContext } from "react";
+import "./CandidateList.scss";
+import CandidateProfile from "../../components/CandidateProfile/CandidateProfile";
 
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-import { candidatesContext } from '../../App'
-
+import { candidatesContext } from "../../App";
 
 const CandidateList = (props) => {
+  const candidates = useContext(candidatesContext);
 
-    const candidates = useContext(candidatesContext)
-
-    return (
-        <>
-            <div className="Header">
-                <div className="Logo">Interview Reports</div>
-                <Link className="login-btn" to='./login'>LogIn</Link>
-            </div>
-            <div className="CandidateContainer">
-                <div className="CandidatesSearch">
-                    <h2>Candidates</h2>
-                    <input className="SearchOne" />
-                </div>
-                <div className="CandidateWrapper">
-                    {candidates && candidates.map(candidate => <CandidateProfile candidate={candidate} />)}
-                </div>
-
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="Header">
+        <div className="Logo">Interview Reports</div>
+        <Link className="login-btn" to="./login">
+          Log in
+        </Link>
+      </div>
+      <div className="CandidateContainer">
+        <div className="CandidatesSearch">
+          <h2>Candidates</h2>
+          <input className="SearchOne" />
+        </div>
+        <div className="CandidateWrapper">
+          {candidates &&
+            candidates.map((candidate) => (
+              <CandidateProfile candidate={candidate} />
+            ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default CandidateList;
--- src/pages/LogIn/LogIn.js
+++ src/pages/LogIn/LogIn.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./LogIn.scss";
-import { Redirect } from 'react-router-dom'
+import { Redirect } from "react-router-dom";
 
 const LogIn = () => {
   const [details, setDetails] = useState({ email: "", password: "" });
@@ -9,59 +9,66 @@ const LogIn = () => {
 
   const [token, setToken] = useState(null);
 
-
-
   const submitHandler = (e) => {
     e.preventDefault();
 
-
     const url = "http://localhost:3333/login";
     fetch(url, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         email: email,
         password: password,
-      })
-    }).then(response => response.json())
-      .then(response => {
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => {
         if (response.accessToken) {
-          setToken(response.accessToken)
-          localStorage.setItem("token", response.accessToken)
+          setToken(response.accessToken);
+          localStorage.setItem("token", response.accessToken);
         } else {
-          throw new Error("Wrong email or password. Please try again!")
+          throw new Error("Wrong email or password. Please try again!");
         }
-      }).catch((error) => {
-        alert(error);
       })
-
-  }
-
-
+      .catch((error) => {
+        alert(error);
+      });
+  };
 
   return (
-
     <form onSubmit={submitHandler}>
       <div className="form-inner">
-        <h2>Login</h2>
+        <h2>Log in</h2>
 
         <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email" id="email" onChange={e =>
-            setDetails({ ...details, email: e.target.value })} value={email} />
+          <label htmlFor="email">Email: </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setDetails({ ...details, email: e.target.value })}
+            value={email}
+          />
         </div>
         <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password" id="password" onChange={e =>
-            setDetails({ ...details, password: e.target.value })} value={password} />
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) =>
+              setDetails({ ...details, password: e.target.value })
+            }
+            value={password}
+          />
         </div>
         <input type="submit" value="LOGIN" onClick={submitHandler} />
         {token && <Redirect to="/reports" />}
       </div>
     </form>
-  )
-}
+  );
+};
 
 export default LogIn;
